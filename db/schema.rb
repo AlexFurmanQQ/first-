6b008c89fe350dc2516cee70cac36fff46490e8f
Alex <alexander310718@gmail.com>
2023-05-07 20:37:33 +0600

    model Category

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_01_081421) do
+ActiveRecord::Schema.define(version: 2023_05_07_135009) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "value"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,6 @@
         )
     end
 end
+Category.create(
+    title: {"Спорт","Кино","Музыка"}
+)
